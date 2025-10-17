340b38ab4c2c84a29e27313389ea7cc0c43ea07f
Faruq Khan <150416913+FaruqKhan11148@users.noreply.github.com>
2025-10-17 08:53:27 +0530

    Update reddit.js

--- backend/api/reddit.js
+++ backend/api/reddit.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       throw new Error(`Reddit fetch failed with status: ${redditRes.status}`);
     }
 
-    const data = await redditRes.json();
+    // const data = await redditRes.json();
     res.status(200).json(data);
 
   } catch (err) {
